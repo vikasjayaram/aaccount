8d971967794c51b3f3b8084d2a464145dbc59c5b
Vikas Jayaram <vikas.ramasethu@gmail.com>
2017-03-01 18:13:05 +1100

    include auth0 passport strategy

--- src/app.js
+++ src/app.js
@@ -1,26 +1,66 @@
 const express = require('express'),
       path = require('path'),
+      logger = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser'),
+      session = require('express-session'),
+      dotenv = require('dotenv'),
+      passport = require('passport'),
+      Auth0Strategy = require('passport-auth0'),
       app = express();
+
+dotenv.load();
 
 const routes = require('./routes')
 
+const strategy = new Auth0Strategy({
+    domain:       process.env.AUTH0_DOMAIN,
+    clientID:     process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  }, (accessToken, refreshToken, extraParams, profile, done) => {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    profile.extraParams = extraParams;
+    return done(null, profile);
+  });
 
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'shhhhhhhhh',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use( (req,res,next)=>{
   res.locals.getBundleUrl = function(bundleName) {
+  	console.log(bundleName);
     return `/assets/${bundleName}.bundle.js`;
   }
   next();
 });
 
 app.use('/',routes);
-
-
-
-
-
 
 module.exports = app;
